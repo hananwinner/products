3d679d87d367efca6e8a2d737f6b0279e01991bf
Hanan Wiener <hanan888@gmail.com>
2018-09-06 16:08:15 +0300

    more tests

--- test/dal/test_page_and_load.py
+++ test/dal/test_page_and_load.py
@@ -10,30 +10,31 @@ unique_entries = test_products["unique_entries"]
 data = test_products["data"]
 
 
-class TestProducts(unittest.TestCase):
+with open("test/dal/new_load.json") as fdr:
+    new_load = json.loads(fdr.read())
+
+
+class TestPageAndLoad(unittest.TestCase):
     def setUp(self):
         dal_products.connect(mongo_config)
         dal_products.clear()
         [dal_products.process_new_entry(p) for p in data]
         self.assertEqual(len(dal_products.get_products()), unique_entries)
 
-    def test_simple_paging(self):
+    def test_paging_during_load(self):
         page = dal_products.get_products(count=2)
         self.assertEqual(len(page), 2)
         self.assertEqual(page[0]["sku"], 42534)
-        page = dal_products.get_products(after=64124,count=2)
+        self.assertEqual(page[1]["sku"], 64124)
+        page = dal_products.get_products(after=64124, count=2)
         self.assertEqual(len(page), 2)
         self.assertEqual(page[0]["sku"], 65908)
         self.assertEqual(page[1]["sku"], 81263)
-        page = dal_products.get_products(before=1070373, count=3)
-        self.assertEqual(len(page), 3)
-        self.assertEqual(page[0]["sku"], 81263)
-        self.assertEqual(page[1]["sku"], 99915)
-
-    def test_by_producer(self):
-        page = dal_products.get_products(producer="America's Asparagus")
-        self.assertEqual(len(page), 1)
-        self.assertEqual(page[0]["sku"], 42534)
+        dal_products.process_new_entry(new_load)
+        page = dal_products.get_products(before=65908, count=2)
+        self.assertEqual(len(page), 2)
+        self.assertEqual(page[0]["sku"], 42535)
+        self.assertEqual(page[1]["sku"], 64124)
 
 
     def tearDown(self):
